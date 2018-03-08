d5fea58d4afd88e28ea613dff96237176be4d773
Bryan Konowitz <bryan@kono.sh>
2018-03-07 23:23:23 -0800

    adding ssh-agent and more git stuff

---
+++ __ssh_agent_init.fish
@@ -0,0 +1,18 @@
+function __ssh_agent_init
+	if [ -n "$SSH_AGENT_PID" ] 
+    ps -ef | grep $SSH_AGENT_PID | grep ssh-agent > /dev/null
+    if [ $status -eq 0 ]
+        __ssh_test_identities
+    end  
+else
+    if [ -f $SSH_ENV ]
+        . $SSH_ENV > /dev/null
+    end  
+    ps -ef | grep $SSH_AGENT_PID | grep -v grep | grep ssh-agent > /dev/null
+    if [ $status -eq 0 ]
+        __ssh_test_identities
+    else 
+        __ssh_start_agent
+    end  
+end
+end
---
+++ __ssh_start_agent.fish
@@ -0,0 +1,10 @@
+function __ssh_start_agent
+	function start_agent                                                                                                                                                                    
+    echo "Initializing new SSH agent ..."
+    ssh-agent -c | sed 's/^echo/#echo/' > $SSH_ENV
+    echo "succeeded"
+    chmod 600 $SSH_ENV 
+    . $SSH_ENV > /dev/null
+    ssh-add
+end
+end

d0a0da81cb87f19848c4759d372318b4a5eebe12
Bryan Konowitz <bryan@kono.sh>
2018-03-15 23:33:50 -0700

    WIP

--- __ssh_agent_init.fish
+++ __ssh_agent_init.fish
@@ -1,18 +1,13 @@
 function __ssh_agent_init
-	if [ -n "$SSH_AGENT_PID" ] 
-    ps -ef | grep $SSH_AGENT_PID | grep ssh-agent > /dev/null
-    if [ $status -eq 0 ]
-        __ssh_test_identities
-    end  
-else
-    if [ -f $SSH_ENV ]
-        . $SSH_ENV > /dev/null
-    end  
+  if [ -f $SSH_ENV ]
+    . $SSH_ENV > /dev/null
     ps -ef | grep $SSH_AGENT_PID | grep -v grep | grep ssh-agent > /dev/null
     if [ $status -eq 0 ]
         __ssh_test_identities
     else 
         __ssh_start_agent
     end  
-end
+  else
+    __ssh_start_agent
+  end
 end
--- __ssh_start_agent.fish
+++ __ssh_start_agent.fish
@@ -1,10 +1,8 @@
 function __ssh_start_agent
-	function start_agent                                                                                                                                                                    
-    echo "Initializing new SSH agent ..."
-    ssh-agent -c | sed 's/^echo/#echo/' > $SSH_ENV
-    echo "succeeded"
-    chmod 600 $SSH_ENV 
-    . $SSH_ENV > /dev/null
-    ssh-add
-end
+  echo "Initializing new SSH agent ..."
+  ssh-agent -c | sed 's/^echo/#echo/' > $SSH_ENV
+  echo "succeeded"
+  chmod 600 $SSH_ENV 
+  . $SSH_ENV > /dev/null
+  ssh-add
 end

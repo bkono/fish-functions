dba098a5f85af21a89de79a633901ab9f6c54410
Bryan Konowitz <bryan@kono.sh>
2018-12-08 16:11:28 -0800

    updating aws-vault functions and abbr

--- __aws_aliases.fish
+++ __aws_aliases.fish
@@ -1,1 +1,15 @@
-abbr ave 'aws-vault exec'
+abbr avl 'aws-vault login '
+
+function ave -d 'aws-vault exec using AWS_VAULT_PROFILE'
+    set profile
+    set cmd
+    if set -q AWS_VAULT_PROFILE
+        set profile $AWS_VAULT_PROFILE
+        set cmd $argv
+    else
+        set profile $argv[1]
+        set cmd $argv[2..-1]
+    end
+
+    aws-vault exec $profile -- $cmd
+end

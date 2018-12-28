d8b6795e35dd0ebaa4d6ad1542b4b8f188f27f8a
Bryan Konowitz <bryan@kono.sh>
2018-12-27 17:29:16 -0800

    adding aws-vault server function

--- __aws_aliases.fish
+++ __aws_aliases.fish
@@ -13,3 +13,15 @@ function ave -d 'aws-vault exec using AWS_VAULT_PROFILE'
 
     aws-vault exec $profile -- $cmd
 end
+
+function avs -d 'aws-vault metadata server using AWS_VAULT_PROFILE'
+    set profile
+    if set -q AWS_VAULT_PROFILE
+        set profile $AWS_VAULT_PROFILE
+    else
+        set profile $argv[1]
+    end
+
+    echo "Starting aws-vault metadata server for profile: $profile"
+    aws-vault exec $profile --server
+end
